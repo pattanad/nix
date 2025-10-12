3b65fe4b0a914f71bab9604c49484a9c7398b6ea
Pavan Pattanada <pattanad@amazon.com>
2025-10-12 05:54:41 -0700

    Initial home-manager configuration

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Home Manager config (minimal)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; # pick a channel or revision you prefer
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      homeConfigurations = {
+        # Use your macOS username here (exact).
+        pattanad = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ({ config, pkgs, ... }: {
+              programs.home-manager.enable = true;
+              
+              # Required: specify the state version
+              home.stateVersion = "24.05";
+              
+              # Set your username and home directory
+              home.username = "pattanad";
+              home.homeDirectory = "/Users/pattanad";
+
+              # Example: install wezterm & neovim by default
+              home.packages = [
+                pkgs.wezterm
+                pkgs.neovim
+                pkgs.tmux
+                pkgs.git
+                pkgs.direnv
+              ];
+
+              programs.direnv.enable = true;
+            })
+          ];
+        };
+      };
+    };
+}
+
