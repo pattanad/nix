d51c15032f0325bb341a4d2af54a593f356b91ac
Pavan Pattanada <pattanad@amazon.com>
2026-02-03 12:07:40 -0800

    Migrate to nix-darwin with Home Manager module
    - Split config into flake.nix (darwin) and home.nix (HM)
    - nix-darwin now manages /etc/zshrc (survives macOS upgrades)
    - Removed delta from packages (managed by programs.delta)

--- flake.nix
+++ flake.nix
@@ -1,47 +1,36 @@
 {
-  description = "Home Manager config (minimal)";
+  description = "nix-darwin + Home Manager config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      homeConfigurations = {
-        # Use your macOS username here (exact).
-        pattanad = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ({ config, pkgs, ... }: {
-              programs.home-manager.enable = true;
-              
-              # Required: specify the state version
-              home.stateVersion = "24.05";
-              
-              # Set your username and home directory
-              home.username = "pattanad";
-              home.homeDirectory = "/Users/pattanad";
-
-              # Example: install packages
-              home.packages = [
-                pkgs.neovim
-                pkgs.tmux
-                pkgs.git
-                pkgs.direnv
-              ];
-
-              programs.direnv.enable = true;
-            })
-          ];
-        };
-      };
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ... }: {
+    darwinConfigurations."842f57a680ea" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ({ pkgs, ... }: {
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          nix.settings.experimental-features = [ "nix-command" "flakes" ];
+          programs.zsh.enable = true;
+          system.stateVersion = 5;
+        })
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          users.users.pattanad.home = "/Users/pattanad";
+          home-manager.users.pattanad = import ./home.nix;
+        }
+      ];
     };
+  };
 }
-
